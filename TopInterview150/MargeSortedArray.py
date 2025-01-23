96d5d9aa9704f184e0df0712d68b43b7af9bc5aa
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-22 22:47:52 -0700

    Merge array exercise

--- TopInterview150/MargeSortedArray.py
+++ TopInterview150/MargeSortedArray.py
@@ -3,9 +3,6 @@ class Solution:
         p1 = m-1
         p2 = n-1
         p = m + n - 1
-    while p1 >= 0 and p2 >= 0:
+    while p1 >= 0 and p2 >=0:
         if nums1[p1] > nums2[p2]:
-            nums1[1] = nums1[p1]
-            p1 -= 1
-        else: 
-            nums1[]
+            
