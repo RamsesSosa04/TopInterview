e15ec24188fad35da162fa5f758e663a4a2e764a
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-24 23:44:12 -0700

    Roman to integer number exercise

--- TopInterview150/RomanToInteger.py
+++ TopInterview150/RomanToInteger.py
@@ -1,12 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman_to_int = {
-            'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C',100,
-            'D',500,
-            'M',1000
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
         }
         total = 0 
+        valor_anterior = 0
+        
+        for char in reversed(s):
+            value = roman_to_int[char]
+            if value < valor_anterior:
+                total -= value
+            else:
+                total += value
+            valor_anterior = value
+        return total
