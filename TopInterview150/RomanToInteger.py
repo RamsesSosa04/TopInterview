4d8c1342e0e65240605a3b6b0d9780c2c26de809
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-24 23:37:31 -0700

    Roman to integer number exercise

--- TopInterview150/RomanToInteger.py
+++ TopInterview150/RomanToInteger.py
@@ -1,3 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        roman_to_int = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C',100,
+            'D',500,
+            'M',1000
+        }
+        total = 0 
