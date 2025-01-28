1767fc5f436376f9fd34053eb2144bb801ef7df9
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-27 22:47:26 -0700

    Longest common prefix exercises

--- TopInterview150/lengthOfLastWord.py
+++ TopInterview150/lengthOfLastWord.py
@@ -1,6 +1,6 @@
+#esta es una app para saber cual es la frase mas larga de la frase
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s = s.strip()
         word = s.split()
         return len(word[-1])
-    
