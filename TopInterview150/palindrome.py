6449508a1ed97210865e23008d076fac36abe4ce
RamsesSosa04 <sosaramses08@gmail.com>
2025-02-03 20:03:59 -0700

    Is palindrome? exercise

--- TopInterview150/palindrome.py
+++ TopInterview150/palindrome.py
@@ -1,3 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+    filtered_chars = [char.lower() for char in s if char.isalnum()]
+    
+    # Unir los caracteres filtrados en una cadena
+    filtered_string = ''.join(filtered_chars)
+    
+    # Comparar la cadena filtrada con su reverso
+    return filtered_string == filtered_string[::-1]
