f034b81e630db64ef2e43bff3b71d1c6f5a33140
RamsesSosa04 <sosaramses08@gmail.com>
2025-02-06 22:28:20 -0700

    palindrome exercise

--- TopInterview150/palindrome.py
+++ TopInterview150/palindrome.py
@@ -1,10 +1,9 @@
 #Palindrome
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # Filtrar los caracteres alfanuméricos y convertir a minúsculas
+
         filtered_chars = [char.lower() for char in s if char.isalnum()]
         
-        # Unir los caracteres filtrados en una cadena
         filtered_string = ''.join(filtered_chars)
         
 
