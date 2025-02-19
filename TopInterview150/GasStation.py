a212501d801ae3f556de5b0125a335dd6c319228
RamsesSosa04 <sosaramses08@gmail.com>
2025-02-18 22:43:02 -0700

    Gas Station Exercise

--- TopInterview150/GasStation.py
+++ TopInterview150/GasStation.py
@@ -1,3 +1,7 @@
 #134. Gas Station
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        total = 0
+        tank = 0
+        start = 0
