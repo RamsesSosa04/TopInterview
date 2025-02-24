a14240533d653f52e3bf4f53a880326dbf59488a
RamsesSosa04 <sosaramses08@gmail.com>
2025-02-23 22:58:28 -0700

    Agregando two sum

--- TopInterview150/TwoSum.py
+++ TopInterview150/TwoSum.py
@@ -1,14 +1,3 @@
 #Two sum
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        
-        for i, num in enumerate(nums):
-            complement = target - num
-            
-            if complement in map:
-                return [map[complement], 1]
-            
-            map[num] = 1
-            
-        return []
