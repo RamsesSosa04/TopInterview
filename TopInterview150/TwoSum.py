1c5cf28ed6501cb3330a1b0c13f1b8a9f4e1b0ad
RamsesSosa04 <sosaramses08@gmail.com>
2025-02-23 23:02:07 -0700

    Agregando two sum

--- TopInterview150/TwoSum.py
+++ TopInterview150/TwoSum.py
@@ -3,11 +3,13 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
     
+        # Recorremos el arreglo
         for i, num in enumerate(nums):
             complement = target - num
             
+            # Verificamos si el complemento ya está en el hash map
             if complement in map:
-                return [map[complement], i]  
+                return [map[complement], i]  # Devolvemos los índices
             
             # Si no, almacenamos el número actual en el hash map
             map[num] = i
