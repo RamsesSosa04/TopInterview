e2d62ad41f8907f611e15cd878b959290a299ea5
RamsesSosa04 <sosaramses08@gmail.com>
2025-02-26 22:01:20 -0700

    Rotate array exercise

--- TopInterview150/RotateArray.py
+++ TopInterview150/RotateArray.py
@@ -12,3 +12,6 @@ class Solution:
                 nums[start], nums[end] = nums[end], nums[start]
                 start += 1
                 end -= 1
+    reverse(0, n-1)    # Invertir todo el arreglo
+    reverse(0, k-1)    # Invertir los primeros k elementos
+    reverse(k, n-1)    # Invertir los últimos n-k elementos
