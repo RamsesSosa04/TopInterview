0f3bb63b82d18901060b68e5499e9cc9d1eac88b
RamsesSosa04 <sosaramses08@gmail.com>
2025-02-27 23:09:11 -0700

    Two sum exercise

--- TopInterview150/TwoSum.py
+++ TopInterview150/TwoSum.py
@@ -2,7 +2,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
-    
         # Recorremos el arreglo
         for i, num in enumerate(nums):
             complement = target - num
