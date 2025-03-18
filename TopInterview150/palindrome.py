8b3a49353743ddeeba269cb9bab1a8559fbe7b4a
RamsesSosa04 <sosaramses08@gmail.com>
2025-03-17 23:51:51 -0700

    Palindrome exercise

--- TopInterview150/palindrome.py
+++ TopInterview150/palindrome.py
@@ -3,3 +3,4 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         filtered_chars = [char.lower() for char in s if char.isalnum()]
         
+        filtered_string = ''.join(filtered_chars)
