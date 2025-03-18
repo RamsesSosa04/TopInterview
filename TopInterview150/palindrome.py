b99c97ddcb4bd996c02ad398dc4cd5a68e9f0330
RamsesSosa04 <sosaramses08@gmail.com>
2025-03-17 23:52:16 -0700

    Palindrome exercise

--- TopInterview150/palindrome.py
+++ TopInterview150/palindrome.py
@@ -4,3 +4,6 @@ class Solution:
         filtered_chars = [char.lower() for char in s if char.isalnum()]
         
         filtered_string = ''.join(filtered_chars)
+        return filtered_string == filtered_string[::-1]
+sol = Solution()
+print(sol.isPalindrome("windows"))  
