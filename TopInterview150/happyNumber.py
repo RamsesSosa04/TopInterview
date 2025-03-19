bd86f91b668ef39e7f015948a06ec2f1cb806d14
RamsesSosa04 <sosaramses08@gmail.com>
2025-03-18 22:10:24 -0700

    Happy number exercise

--- TopInterview150/happyNumber.py
+++ TopInterview150/happyNumber.py
@@ -1,16 +1,1 @@
 #Happy number
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        
-        def sum_of_squares(number):
-            total = 0
-            while number > 0:
-                digit = number % 10
-                total += digit ** 2
-                number //= 10
-            return total
-        seen = set()
-        while n != 1 and n not in seen:
-            seen.add(n)
-            n = sum_of_squares(n)
-        return n == 1
