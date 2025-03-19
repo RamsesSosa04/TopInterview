4d3449b932e371f3161caf28688f1c15a4f11e76
RamsesSosa04 <sosaramses08@gmail.com>
2025-03-18 22:23:17 -0700

    Happy number exercise

--- TopInterview150/happyNumber.py
+++ TopInterview150/happyNumber.py
@@ -2,3 +2,9 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         
+        def sum_of_squares(number):
+            total = 0
+            while number > 0:
+                digit = number % 10
+                total += digit ** 2
+                number //= 10
