6a5088dc4a7418412c2fb347d4694f5fc63e426b
RamsesSosa04 <sosaramses08@gmail.com>
2025-03-22 23:54:53 -0700

    Length of last word exercise

--- TopInterview150/lengthOfLastWord.py
+++ TopInterview150/lengthOfLastWord.py
@@ -1,8 +1,2 @@
 #Esta es una app para saber cual es la frase mas larga de la frase
 
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        s = s.strip()
-        word = s.split()
-        return len(word[-1])
-    
