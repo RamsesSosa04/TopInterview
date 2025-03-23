e6954455537a5f222e4c1030edaca795ea098115
RamsesSosa04 <sosaramses08@gmail.com>
2025-03-22 23:53:24 -0700

    Best time to buy exercise

--- TopInterview150/BestTimeToBuy.py
+++ TopInterview150/BestTimeToBuy.py
@@ -1,12 +1,2 @@
 #Best Time to Buy and Sell Stock
-class Solution:
-    def maxProfit(self, prices: list[int]) -> int:
-        min = float('inf')
-        max_profit = 0
-        
-        for price in prices:
-            if price < min:
-                min = price
-            else:
-                max_profit = max(max_profit, price - min)
-        return max_profit    
+ 
