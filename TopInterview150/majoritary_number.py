b797eebc9e3cc321fed92c944a8eaef141578beb
RamsesSosa04 <sosaramses08@gmail.com>
2025-03-22 23:56:12 -0700

    majoritary number exercise

--- TopInterview150/majoritary_number.py
+++ TopInterview150/majoritary_number.py
@@ -1,20 +1,3 @@
 from typing import List
 
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        candidate = None
         
-        for num in nums:
-            if count == 0:
-                candidate = num
-        count +=1 if num == candidate else -1
-        
-        count = 0
-        for num in nums:
-            if num == candidate:
-                count += 1
-        if count > len(nums) // 2:        
-            return candidate
-        else:
-            raise ValueError("No hay elemento mayoritario válido")            
