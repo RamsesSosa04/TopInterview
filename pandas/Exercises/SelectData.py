7444f39b6cf6939e0343f6a91c6a3de049e48c2a
RamsesSosa04 <sosaramses08@gmail.com>
2025-03-28 19:21:15 -0700

    added select data execise

--- pandas/Exercises/SelectData.py
+++ pandas/Exercises/SelectData.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
-def select_student(students : pd.DataFrame):
-    mask = students['student_id']==101
-    filter = students[mask]
+def select_student(students):
+    df = pd.DataFrame(students)
+    mask = df['student_id']==101
+    filter = df[mask]
     result = filter[['name', 'age']]
     return result
+students = [
+    {"student_id" : 101, "name" : "Ulises", "age" : 29},
+    {"student_id" : 102, "name" : "Ramses", "age" : 20},
+    {"student_id" : 103, "name" : "Aurelio", "age" : 53}
+]
+print(select_student(students))
