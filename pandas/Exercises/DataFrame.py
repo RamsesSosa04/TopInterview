de3a75966c626c82fffb0790fa57cffd117aaa8c
RamsesSosa04 <sosaramses08@gmail.com>
2025-03-29 23:51:39 -0700

    added Data frame execise

--- pandas/Exercises/DataFrame.py
+++ pandas/Exercises/DataFrame.py
@@ -3,3 +3,10 @@ import pandas as pd
 def createDataframe(student_data) -> pd.DataFrame:
     df = pd.DataFrame(student_data, columns=['student_id', 'age'])
     return df
+
+student_data = [
+    [1, 12],
+    [2, 14],
+    [3, 13],
+    [4, 12]
+] 
