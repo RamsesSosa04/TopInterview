e5333bfc62d01c1e176e7545a373c1e6622b67e9
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-02 23:57:02 -0700

    Added drop missing exercise

--- pandas/Exercises/DropMissing.py
+++ pandas/Exercises/DropMissing.py
@@ -2,3 +2,12 @@ import pandas as pd
 
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
     return students.dropna(subset=['name'])
+
+data = {
+    'student_id': [32, 217, 779, 849],
+    'name': ['Piper', None, 'Georgia', 'Willow'],
+    'age': [5, 19, 20, 14]
+}
+students = pd.DataFrame(data)
+
+print(dropMissingData(students).to_string)
