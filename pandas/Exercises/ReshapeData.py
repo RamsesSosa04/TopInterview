251636eb1a1e687fed77ba611e9c06384e0992ac
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-07 23:41:16 -0700

    added Reshape Data exercise

--- pandas/Exercises/ReshapeData.py
+++ pandas/Exercises/ReshapeData.py
@@ -2,3 +2,18 @@ import pandas as pd
 
 def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     return pd.concat([df1, df2], axis=0)
+data1 = {
+    'student_id': [1, 2, 3, 4],
+    'name': ['Mason', 'Ava', 'Taylor', 'Georgia'],
+    'age': [8, 6, 15, 17]
+}
+
+data2 = {
+    'student_id': [5, 6],
+    'name': ['Leo', 'Alex'],
+    'age': [7, 7]
+}
+df1 = pd.DataFrame(data1)
+df2 = pd.DataFrame(data2)
+
+print(concatenateTables(df1, df2)) 
