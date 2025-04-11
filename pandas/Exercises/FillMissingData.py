0f14968708f5799b56f92255cf0899fa866263e1
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-10 23:26:29 -0700

    Fill Missing Data exercise

--- pandas/Exercises/FillMissingData.py
+++ pandas/Exercises/FillMissingData.py
@@ -3,3 +3,8 @@ import pandas as pd
 def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
     products['quantity'] = products['quantity'].fillna(0) #is to convert all null data to the value of your choice
     return products
+data = {
+    'name': ['Wristwatch', 'WirelessEarbuds', 'GolfClubs', 'Printer'],
+    'quantity': [None, None, 779, 849],
+    'price': [135, 821, 9319, 3051]    
+}
