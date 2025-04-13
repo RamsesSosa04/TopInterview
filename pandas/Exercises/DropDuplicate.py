79d72db1119a4d2edb9f7c5e8361aecaadfb8b77
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-12 23:53:28 -0700

    Drop duplicate exercise

--- pandas/Exercises/DropDuplicate.py
+++ pandas/Exercises/DropDuplicate.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
     return customers.drop_duplicates(subset=['email'], keep='first')
+data = {
+    'customer_id': [1, 2, 3, 4, 5, 6],
+    'name': ['Ella', 'David', 'Zachary', 'Alice', 'Finn', 'Violet'],
+    'email': ['emily@gmail.com', 'michael@gmail.com', 'sarah@gmail.com', 
+    'john@gmail.com', 'john@gmail.com', 'alice@gmail.com']
+}
