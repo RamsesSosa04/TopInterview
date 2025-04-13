15587413f0dfff190bedaa82a9cee2796b688bbe
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-12 23:54:56 -0700

    Rename Column exercise

--- pandas/Exercises/RenameColumns.py
+++ pandas/Exercises/RenameColumns.py
@@ -2,3 +2,9 @@
 import pandas as pd
 
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    rename = {
+        'id' : ' students_id',
+        'first' : 'first_name',
+        'last': 'last_name',
+        'age': 'age_in_years'
+    }
