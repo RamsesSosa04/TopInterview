9564f9687aabcd4ec4c075313ef8c5d6367d2008
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-13 23:08:28 -0700

    Method Chaining exercise

--- pandas/Exercises/MethodChaining.py
+++ pandas/Exercises/MethodChaining.py
@@ -2,3 +2,10 @@ import pandas as pd
 
 def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
     return animals[animals['weight'] > 100].sort_values('weight', ascending=False)[['name']]
+
+data = {
+    'name' : ['Tatiana', 'Khaled', 'Alex', 'Jonathan', 'Stefan', 'Tommy'],
+    'species' : ['Snake', 'Giraffe', 'Leopard', 'Monkey', 'Bear', 'Panda'],
+    'age': [98, 50, 6, 45, 100, 26],
+    'weight': [464, 41, 328, 463, 50, 349]
+}
